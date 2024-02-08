96c38cef23c60acac4abb482a7f9e02bb6980846
Philip Spasov <philip.spasov2@gmail.com>
2024-02-08 09:28:15 +0200

    not used logoutbutton

--- Parcel/logoutRedirectionStuff.js
+++ Parcel/logoutRedirectionStuff.js
@@ -9,22 +9,22 @@ window.onload = function () {
         .catch(error => console.error('Error:', error));
 };
 
-document.getElementById('logoutButton').addEventListener('click', function () {
-    fetch('/api/logout', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                window.location.href = '/login.html'; // Redirect to login page after logout
-            } else {
-                alert('Logout failed. Please try again.');
-            }
-        })
-        .catch(error => console.error('Error:', error));
-});
+// document.getElementById('logoutButton').addEventListener('click', function () {
+//     fetch('/api/logout', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//         .then(response => response.json())
+//         .then(data => {
+//             if (data.success) {
+//                 window.location.href = '/login.html'; // Redirect to login page after logout
+//             } else {
+//                 alert('Logout failed. Please try again.');
+//             }
+//         })
+//         .catch(error => console.error('Error:', error));
+// });
 
 // It is used, please don't touch it <3
